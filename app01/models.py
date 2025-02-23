34e7792eca3538369e454a6d0cb354f84eb60f44
Dante-B216 <2916307326@qq.com>
2025-02-23 18:25:13 +0800

    RegisterModelForm页面

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    user_name = models.CharField(verbose_name='用户名', max_length=32)
+    user_email = models.EmailField(verbose_name='邮箱',max_length=32)     # 内置正则表达式
+    user_phone = models.CharField(verbose_name='手机号',max_length=32)
+    user_pw = models.CharField(verbose_name='密码', max_length=32)
+
--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django import forms
+from app01 import models
+
+from django.core.validators import RegexValidator
+
+from io import BytesIO
+from django.http import HttpResponse
+from django.views import View
+from PIL import Image, ImageDraw, ImageFont
+import random
+
+class RegisterModelForm(forms.ModelForm):
+    # 用户名
+    user_name = forms.CharField(label='用户名')
+
+    # 邮箱
+    user_email = forms.EmailField(label='邮箱')
+
+    # 手机号验证
+    user_phone = forms.CharField(label='手机号', validators=[RegexValidator(r'^1[3-9]\d{9}$', '手机号格式错误')])
+
+    # 密码隐藏
+    user_pw = forms.CharField(label='密码', widget=forms.PasswordInput)
+
+    # 二次输入密码
+    confirm_user_pw = forms.CharField(label='重复密码', widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = models.UserInfo
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = '请输入%s' % (field.label)
+
+
+def register(request):
+    form = RegisterModelForm()
+    return render(request, "register.html", {'form': form})
