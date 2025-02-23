518569ce9aa822f611e6e245bf94becb59d254b0
Dante-B216 <2916307326@qq.com>
2025-02-23 16:00:12 +0800

    sms短信发送工具

--- graduationDesign/settings.py
+++ graduationDesign/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-jru(tby&zj3-p+g$$=q6b-f$9hsw2abaaeeotk3#66k0h+fnk1
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'graduationDesign.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -113,7 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -123,6 +117,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ----------sms----------
+# 应用ID
+SMS_APPID = 112142311
+# 应用Key
+SMS_APPKEY = "8cc5b87123y423423412387930004"
+# 腾讯云创建签名时填写的签名内容(使用公众号的话这个值一般是公众号全称或简称)
+SMS_SIGN = "活蹦乱跳的小白"
+
+SMS_TEMPLATE = {
+    'register': 2367219,
+    'login': 2367220
+}
 
 try:
     from .local_settings.py import *
